5142863120d4132865a569b775f87f3b84a810c4
�東�京� <131155313+konomi-tokyo@users.noreply.github.com>
2023-12-06 17:40:19 +0900

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function() { 
 var title = localStorage.getItem("title")
 var favicon = localStorage.getItem("favicon")
 
